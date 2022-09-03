ae2fda890a5e5801c5475ac33322716e7c920819
Rin Okuyama <rokuyama.rk@gmail.com>
2022-09-03 09:25:03 +0900

    Localize addr and buf. NFC.

--- fpu_emu.c
+++ fpu_emu.c
@@ -294,7 +294,6 @@ fpu_execute(struct trapframe *tf, struct fpemu *fe, union instr *insn)
 	struct fpn *fp;
 	union instr instr = *insn;
 	int *a;
-	vaddr_t addr;
 	int ra, rb, rc, rt, type, mask, fsr, cx, bf, setcr, cond;
 	u_int bits;
 	struct fpreg *fs;
@@ -335,7 +334,7 @@ fpu_execute(struct trapframe *tf, struct fpemu *fe, union instr *insn)
 		 * Convert to/from single if needed, calculate addr,
 		 * and update index reg if needed.
 		 */
-		uint64_t buf;
+		vaddr_t addr;
 		size_t size = sizeof(double);
 		int store, update;
 
@@ -412,6 +411,8 @@ fpu_execute(struct trapframe *tf, struct fpemu *fe, union instr *insn)
 			/* Store */
 			FPU_EMU_EVCNT_INCR(fpstore);
 			if (type != FTYPE_DBL) {
+				uint64_t buf;
+
 				DPRINTF(FPE_INSN,
 					("fpu_execute: Store SNG at %p\n",
 						(void *)addr));
